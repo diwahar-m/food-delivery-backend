91371f61a0a7428dae7568a125022d9ac63d1c95
diwahar-m <msdiwahar5@gmail.com>
2024-08-02 22:00:41 +0530

    add food item functionality done

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -6,9 +6,17 @@ import multer from "multer";
 const foodRouter = express.Router();
 
 // Image Storage Engine
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename:(req, file, cb)=>{
+        return cb(null, `${Date.now()}${file.originalname}`) 
+        // this method makes the filename as unique
+    }
+})
+const upload = multer({ storage: storage})
 
 
-foodRouter.post("/add", addFood)
+foodRouter.post("/add",upload.single("image"), addFood)
 
 
 export default foodRouter;
