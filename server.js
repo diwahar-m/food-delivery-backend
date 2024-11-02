cc34173a5c6a4330e29c14a984f3783f032f7c1f
diwahar-m <msdiwahar5@gmail.com>
2024-11-02 21:46:53 +0530

    authentication added for user cart

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import "dotenv/config" // used to get .env variables
+import cartRouter from "./routes/cartRoute.js"
 
 // app config
 const app = express()
@@ -31,7 +32,7 @@ connectDB();
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
-
+app.use("/api/cart", cartRouter)
 
 // Routes
 app.get("/",(req, res)=>{
